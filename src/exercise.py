1ec126caae82fa78b9582cb3abe4c27e1c5752da
Bianca Ruiz <bianca.ruiz@uvu.edu>
2021-02-25 16:32:31 +0000

    completed assignment

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # borra la instrucción 'pass' y escribe tu código aquí abajo:
-    pass
+    num = int(input('Introduzca un número: '))
+    
+    if num % 10 == 0:
+      múltiplo = 'sí'
+    else:
+      múltiplo = 'no'
+
+    print(f'El número {num} {múltiplo} es múltiplo de 10')
 
 
 if __name__ == '__main__': 
