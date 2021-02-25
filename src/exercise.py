0f84ce6230e16a9b448bb215ff39923857d5cb4a
Bianca Ruiz <bianca.ruiz@uvu.edu>
2021-02-25 17:15:48 +0000

    reverted to original solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,13 +1,13 @@
 def main():
     # borra la instrucción 'pass' y escribe tu código aquí abajo:
-    num = int(input('Introduzca un numero: '))
-
+    num = int(input('Introduzca un número: '))
+    
     if num % 10 == 0:
-      múltiplo = 'si'
+      múltiplo = 'sí'
     else:
       múltiplo = 'no'
 
-    print(f'El numero {num} {múltiplo} es multiplo de 10')
+    print(f'El número {num} {múltiplo} es múltiplo de 10')
 
 
 if __name__ == '__main__': 
