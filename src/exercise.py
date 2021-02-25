16577d16a9fa3a6351d3010af70d8b243b1ab62b
Bianca Ruiz <bianca.ruiz@uvu.edu>
2021-02-25 16:59:16 +0000

    testing without special characters.

--- src/exercise.py
+++ src/exercise.py
@@ -1,13 +1,13 @@
 def main():
     # borra la instrucción 'pass' y escribe tu código aquí abajo:
-    num = int(input('Introduzca un número: '))
-    
+    num = int(input('Introduzca un numero: '))
+
     if num % 10 == 0:
-      múltiplo = 'sí'
+      múltiplo = 'si'
     else:
       múltiplo = 'no'
 
-    print(f'El número {num} {múltiplo} es múltiplo de 10')
+    print(f'El numero {num} {múltiplo} es multiplo de 10')
 
 
 if __name__ == '__main__': 
